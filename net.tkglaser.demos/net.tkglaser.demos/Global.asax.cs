fefa2f684800ebacfa97e1160bbec4ee46e37a2a
Thomas Glaser <thomas.glsr@gmail.com>
2012-12-13 22:20:33 +0000

    Init

--- net.tkglaser.demos/net.tkglaser.demos/Global.asax.cs
+++ net.tkglaser.demos/net.tkglaser.demos/Global.asax.cs
@@ -22,7 +22,6 @@ namespace net.tkglaser.demos
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
