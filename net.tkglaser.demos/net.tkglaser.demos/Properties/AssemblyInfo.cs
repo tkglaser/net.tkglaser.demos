2eb42c844b38dcc922b2b1cd5bc096e357227e25
Thomas Glaser <thomas.glsr@gmail.com>
2012-12-13 22:22:43 +0000

    Init

--- net.tkglaser.demos/net.tkglaser.demos/Properties/AssemblyInfo.cs
+++ net.tkglaser.demos/net.tkglaser.demos/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f6a4bafa-12a7-43ec-82ae-45a7120feaa0")]
+[assembly: Guid("a1fab5c4-c3ce-4be0-876b-cc67909d8b2b")]
 
 // Version information for an assembly consists of the following four values:
 //
