41aa3593a219740c89b7dadff25a748620d5b5a9
Thomas Glaser <thomas.glsr@gmail.com>
2012-12-14 00:22:21 +0000

    Phone

---
+++ net.tkglaser.demos/net.tkglaser.demos/Controllers/PhoneValidationController.cs
@@ -0,0 +1,35 @@
+﻿using net.tkglaser.demos.Models.Phone;
+using PhoneNumbers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace net.tkglaser.demos.Controllers
+{
+    public class PhoneValidationController : Controller
+    {
+        public ActionResult EnterDetails()
+        {
+            return View(new DetailsModel());
+        }
+
+        [HttpPost]
+        public ActionResult EnterDetails(DetailsModel model)
+        {
+            try
+            {
+                PhoneNumberUtil phoneUtil = PhoneNumberUtil.GetInstance();
+                PhoneNumber n = phoneUtil.Parse(model.Phone, "GB");
+
+                model.PhoneNumberFormatted = phoneUtil.Format(n, PhoneNumberFormat.INTERNATIONAL);
+            }
+            catch (Exception e)
+            {
+                ModelState.AddModelError("Phone", e.Message);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ net.tkglaser.demos/net.tkglaser.demos/Models/Phone/DetailsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace net.tkglaser.demos.Models.Phone
+{
+    public class DetailsModel
+    {
+        public string Phone { get; set; }
+        public string PhoneNumberFormatted { get; set; }
+    }
+}
