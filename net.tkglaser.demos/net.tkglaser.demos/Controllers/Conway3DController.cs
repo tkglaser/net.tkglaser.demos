15482429f30ff4fad9906e6f1378d7450cd3df74
Thomas Glaser <thomas.glsr@gmail.com>
2012-12-14 22:45:57 +0000

    Demos

---
+++ net.tkglaser.demos/net.tkglaser.demos/Controllers/Conway3DController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace net.tkglaser.demos.Controllers
+{
+    public class Conway3DController : Controller
+    {
+        //
+        // GET: /Conway3D/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ net.tkglaser.demos/net.tkglaser.demos/Controllers/SpinnerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace net.tkglaser.demos.Controllers
+{
+    public class SpinnerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string search)
+        {
+            Thread.Sleep(5000);
+
+            return View();
+        }
+    }
+}
