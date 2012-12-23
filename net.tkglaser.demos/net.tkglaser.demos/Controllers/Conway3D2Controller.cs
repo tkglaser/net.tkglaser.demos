44abe1501b64b702bc9f68024d6b93060786db02
Thomas Glaser <thomas.glsr@gmail.com>
2012-12-23 13:50:07 +0000

    Conway version 2

---
+++ net.tkglaser.demos/net.tkglaser.demos/Controllers/Conway3D2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace net.tkglaser.demos.Controllers
+{
+    public class Conway3D2Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult WebGL()
+        {
+            return View();
+        }
+
+    }
+}
