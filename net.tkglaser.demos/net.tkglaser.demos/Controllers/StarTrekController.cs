f9d1f668011a7f02b80ce602d152696e5f2b5241
Thomas Glaser <support@vorwardit.co.uk>
2016-05-07 10:22:01 +0100

    Star Trek

---
+++ net.tkglaser.demos/net.tkglaser.demos/Controllers/StarTrekController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace net.tkglaser.demos.Controllers
+{
+    public class StarTrekController : Controller
+    {
+        //
+        // GET: /StarTrek/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
